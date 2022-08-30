97cbd9e4712773b0f2bcf298acc974f0661abfbf
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-08-30 11:03:29 +1000

    Create featured products component.

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -18,7 +18,7 @@ const initialState = {
   products_loading: false,
   products_error: false,
   products: [],
-  feature_products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -35,8 +35,13 @@ export const ProductsProvider = ({ children }) => {
 
   const fetchProducts = async (url) => {
     dispatch({ type: GET_PRODUCTS_BEGIN });
-    const response = await axios.get(url);
-    console.log(response);
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
   };
 
   useEffect(() => {
